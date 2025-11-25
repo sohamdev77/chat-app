eb4d93c381ea0dbf9137d63952a21672bafb1798
sohamdev77 <sohom2682009@gmail.com>
2025-11-26 02:44:06 +0530

    Updated socket URL for deployment

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,3 @@
-import { io } from "socket.io-client";
-
-const socket = io("http://localhost:5000", {
+const socket = io("https://chat-app-backend-hpi0.onrender.com", {
     transports: ["websocket"],
 });
-
-export default socket;
